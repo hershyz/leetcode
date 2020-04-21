fda6aa10dc72be1be027cae05b3e3af7d778d6a3
Hrishikesh Bagalkote <bagalkote.family@outlook.com>
2020-04-21 13:14:08 -0400

    Create DecodeWays.java

---
+++ DecodeWays.java
@@ -0,0 +1,54 @@
+/*
+* https://leetcode.com/problems/decode-ways/
+* Difficulty: MEDIUM
+* */
+
+import java.net.Inet4Address;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String s = "12";
+        numDecodings(s);
+    }
+
+    public static void numDecodings(String s) {
+
+        String[] arr = s.split("");
+        int i;
+        for (i = 0; i < arr.length; i++) {
+            int j;
+            for (j = i; j < arr.length; j++) {
+                int index = getIndex(arr, i, j);
+                String alphabetIndex = getAlphabetIndex(index);
+                if (alphabetIndex.length() > 0) {
+                    System.out.print(alphabetIndex.toUpperCase());
+                }
+            }
+            System.out.println("");
+        }
+    }
+
+    public static String getAlphabetIndex(int i) {
+
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+        String[] arr = alphabet.split("");
+        try {
+            return arr[i - 1];
+        }
+        catch (Exception e) {
+            return "";
+        }
+    }
+
+    public static int getIndex(String[] arr, int l, int r) {
+
+        String s = "";
+        int i;
+        for (i = l; i <= r; i++) {
+            s = s + arr[i];
+        }
+
+        return Integer.parseInt(s);
+    }
+}
