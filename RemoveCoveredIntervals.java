1a19e5289f338653c091f8e0673dadbe5830b48d
Hrishikesh Bagalkote <bagalkote.family@outlook.com>
2021-04-19 20:27:22 -0400

    Update RemoveCoveredIntervals.java

--- RemoveCoveredIntervals.java
+++ RemoveCoveredIntervals.java
@@ -1,47 +1,33 @@
-/*
- * https://leetcode.com/problems/remove-covered-intervals/
- * Difficulty: MEDIUM
- * */
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class Main {
-
-    public static void main(String[] args) {
-
-    }
-
-    public static int removeCoveredIntervals(int[][] intervals) {
-
-        List<int[]> ints = new ArrayList<>();
-        for (int[] interval : intervals) {
-            ints.add(interval);
+class Solution {
+    public int removeCoveredIntervals(int[][] intervals) {
+        
+        List<int[]> list = new ArrayList<>();
+        for (int[] i : intervals) {
+            list.add(i);
         }
-
-        for (int i = 0; i < ints.size(); i++) {
-            for (int j = 0; j < ints.size(); j++) {
+        
+        for (int i = 0; i < list.size(); i++) {
+            int[] curr = list.get(i);
+            boolean subtract = false;
+            boolean br = false;
+            for (int j = 0; j < list.size(); j++) {
+                if (br) {
+                    break;
+                }
                 if (j != i) {
-                    int[] int1 = ints.get(i);
-                    int[] int2 = ints.get(j);
-                    if (test(int1, int2)) {
-                        ints.remove(i);
+                    int[] test = list.get(j);
+                    if (test[0] <= curr[0] && test[1] >= curr[1]) {
+                        list.remove(i);
+                        subtract = true;
+                        br = true;
                     }
                 }
             }
-        }
-
-        return ints.size();
-    }
-
-    public static boolean test(int[] int1, int[] int2) {
-
-        if (int1[0] >= int2[0]) {
-            if (int1[1] <= int2[1]) {
-                return true;
+            if (subtract) {
+                i--;
             }
         }
-
-        return false;
+        
+        return list.size();
     }
 }
