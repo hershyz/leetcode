f4f7c1486262a4d368333494a312d25c816e50f1
Hrishikesh Bagalkote <bagalkote.family@outlook.com>
2021-04-22 01:05:18 -0400

    Update ShortestUnsortedContinuousSubarray.java

--- ShortestUnsortedContinuousSubarray.java
+++ ShortestUnsortedContinuousSubarray.java
@@ -1,103 +1,45 @@
-/*
- * https://leetcode.com/problems/shortest-unsorted-continuous-subarray/
- * Difficulty: EASY
- * */
-
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.ArrayList;
-
-public class Main {
-
-    public static void main(String[] args) {
-        int[] nums = new int[]{5, 4, 3, 2, 1};
-        System.out.println(findUnsortedContinuousSubarray(nums));
-    }
-
-    public static int findUnsortedContinuousSubarray(int[] nums) {
+class Solution {
+    public static int findUnsortedSubarray(int[] nums) {
 
         if (isSorted(nums)) {
             return 0;
         }
 
-        int shortest = 10000;
+        int min = nums.length;
 
-        int i;
-        int j;
-        for (i = 0; i < nums.length - 1; i++) {
-            for (j = i + 1; j < nums.length; j++) {
-
-                int[] arr = getArray(nums, i, j);
-                Arrays.sort(arr);
-                List<Integer> list = new ArrayList<>();
-
-                for (int x = 0; x < i; x++) {
-                    list.add(nums[x]);
-                }
-                for (int num : arr) {
-                    list.add(num);
-                }
-                if (j < nums.length - 1) {
-                    for (int y = (j + 1); y < nums.length; y++) {
-                        list.add(nums[y]);
-                    }
-                }
-
-                List<Integer> test = new ArrayList<>();
-                for (Integer num : list) {
-                    test.add(num);
-                }
-
-                Collections.sort(test);
-                if (compare(test, list)) {
-                    int length = j - i + 1;
-                    if (length < shortest) {
-                        shortest = length;
-                    }
-                }
-            }
-        }
-
-        return shortest;
-    }
-
-    public static int[] getArray(int[] nums, int i, int j) {
-
-        int[] arr = new int[(j - i) + 1];
-        int index = 0;
-        for (int x = i; x <= j; x++) {
-            arr[index] = nums[x];
-            index++;
-        }
-
-        return arr;
-    }
-
-    public static boolean compare(List<Integer> a, List<Integer> b) {
-
-        for (int i = 0; i < a.size(); i++) {
-            if (a.get(i) != b.get(i)) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    public static boolean isSorted(int[] nums) {
-        int[] test = new int[nums.length];
-        int index = 0;
-        for (int num : nums) {
-            test[index] = num;
-            index++;
-        }
-        Arrays.sort(test);
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != test[i]) {
+            for (int j = i; j < nums.length; j++) {
+
+                int[] test = new int[nums.length];
+                int index = 0;
+
+                for (int n : nums) {
+                    test[index] = n;
+                    index++;
+                }
+
+                Arrays.sort(test, i, j + 1);
+
+                if (isSorted(test)) {
+                    int length = (j - i) + 1;
+                    if (length < min) {
+                        min = length;
+                    }
+                }
+            }
+        }
+
+        return min;
+    }
+
+    public static boolean isSorted(int[] arr) {
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < arr[i - 1]) {
                 return false;
             }
         }
+
         return true;
     }
 }
